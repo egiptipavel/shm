2b93cd19eb848e6767d9d67a62af1ce599d5c27b
Pavel Egipti <kinqstone585@gmail.com>
2025-03-23 20:00:09 +0300

    Refactor creating of commands

--- internal/notifier/notifier.go
+++ internal/notifier/notifier.go
@@ -37,12 +37,12 @@ func NewTGBot(token string, stor *storage.Storage) (*TGBot, error) {
 		storage: stor,
 	}
 
-	bot.Handle("/start", t.startCommand())
-	bot.Handle("/subscribe", t.subscribeCommand())
-	bot.Handle("/unsubscribe", t.unsubscribeCommand())
-	bot.Handle("/add", t.addSiteCommand())
-	bot.Handle("/delete", t.deleteSiteCommand())
-	bot.Handle("/list", t.listCommand())
+	bot.Handle("/start", t.startCommand)
+	bot.Handle("/subscribe", t.subscribeCommand)
+	bot.Handle("/unsubscribe", t.unsubscribeCommand)
+	bot.Handle("/add", t.addSiteCommand)
+	bot.Handle("/delete", t.deleteSiteCommand)
+	bot.Handle("/list", t.listCommand)
 
 	return t, nil
 }
@@ -115,151 +115,139 @@ func (t *TGBot) notifySubscribers(url string, message string) error {
 	return nil
 }
 
-func (t *TGBot) startCommand() func(c telebot.Context) error {
-	return func(c telebot.Context) error {
-		slog.Info("start command", slog.Int64("chat_id", c.Chat().ID))
-		return c.Send(`Commands:
+func (t *TGBot) startCommand(c telebot.Context) error {
+	slog.Info("start command", slog.Int64("chat_id", c.Chat().ID))
+	return c.Send(`Commands:
 	/subscribe - subscribe to updates
 	/unsubscribe - unsubscribe from updates
 	/add [url] - start monitoring [url] site
 	/delete [url] - stop monitoring [url] site
 	/list - get all monitored sites
 	`)
-	}
 }
 
-func (t *TGBot) subscribeCommand() func(c telebot.Context) error {
-	return func(c telebot.Context) error {
-		slog.Info("subscribe command", slog.Int64("chat_id", c.Chat().ID))
+func (t *TGBot) subscribeCommand(c telebot.Context) error {
+	slog.Info("subscribe command", slog.Int64("chat_id", c.Chat().ID))
 
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
-		if err := t.storage.AddChat(ctx, storage.Chat{Id: c.Chat().ID}); err != nil {
-			slog.Error(
-				"failed to add chat",
-				slog.String("command", "subscribe"),
-				slog.String("error", err.Error()),
-			)
-			return nil
-		}
-
-		return c.Send("Successful!")
+	if err := t.storage.AddChat(ctx, storage.Chat{Id: c.Chat().ID}); err != nil {
+		slog.Error(
+			"failed to add chat",
+			slog.String("command", "subscribe"),
+			slog.String("error", err.Error()),
+		)
+		return nil
 	}
+
+	return c.Send("Successful!")
 }
 
-func (t *TGBot) unsubscribeCommand() func(c telebot.Context) error {
-	return func(c telebot.Context) error {
-		slog.Info("unsubscribe command", slog.Int64("chat_id", c.Chat().ID))
+func (t *TGBot) unsubscribeCommand(c telebot.Context) error {
+	slog.Info("unsubscribe command", slog.Int64("chat_id", c.Chat().ID))
 
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
-		if err := t.storage.UpdateChat(ctx, c.Chat().ID, false); err != nil {
-			slog.Error(
-				"failed to update chat",
-				slog.String("command", "unsubscribe"),
-				slog.String("error", err.Error()),
-			)
-			return nil
-		}
-
-		return c.Send("Successful!")
+	if err := t.storage.UpdateChat(ctx, c.Chat().ID, false); err != nil {
+		slog.Error(
+			"failed to update chat",
+			slog.String("command", "unsubscribe"),
+			slog.String("error", err.Error()),
+		)
+		return nil
 	}
+
+	return c.Send("Successful!")
 }
 
-func (t *TGBot) addSiteCommand() func(c telebot.Context) error {
-	return func(c telebot.Context) error {
-		chatId := c.Chat().ID
-		url := c.Message().Payload
+func (t *TGBot) addSiteCommand(c telebot.Context) error {
+	chatId := c.Chat().ID
+	url := c.Message().Payload
 
-		slog.Info("add site command", slog.Int64("chat_id", chatId), slog.String("url", url))
+	slog.Info("add site command", slog.Int64("chat_id", chatId), slog.String("url", url))
 
-		url, err := urlpkg.ConvertToExpectedUrl(url)
-		if err != nil {
-			slog.Error(
-				"failed to convert url to expected",
-				slog.String("error", err.Error()),
-				slog.String("url", url),
-			)
-			return c.Reply("Invalid URL!")
-		}
-
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
-
-		if err := t.storage.AddSite(ctx, chatId, url); err != nil {
-			slog.Error(
-				"failed to add site",
-				slog.String("command", "add site"),
-				slog.String("error", err.Error()),
-			)
-			return nil
-		}
-
-		return c.Send("Successful!")
+	url, err := urlpkg.ConvertToExpectedUrl(url)
+	if err != nil {
+		slog.Error(
+			"failed to convert url to expected",
+			slog.String("error", err.Error()),
+			slog.String("url", url),
+		)
+		return c.Reply("Invalid URL!")
 	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := t.storage.AddSite(ctx, chatId, url); err != nil {
+		slog.Error(
+			"failed to add site",
+			slog.String("command", "add site"),
+			slog.String("error", err.Error()),
+		)
+		return nil
+	}
+
+	return c.Send("Successful!")
 }
 
-func (t *TGBot) deleteSiteCommand() func(c telebot.Context) error {
-	return func(c telebot.Context) error {
-		chatId := c.Chat().ID
-		url := c.Message().Payload
+func (t *TGBot) deleteSiteCommand(c telebot.Context) error {
+	chatId := c.Chat().ID
+	url := c.Message().Payload
 
-		slog.Info("delete site command", slog.Int64("chat_id", chatId), slog.String("url", url))
+	slog.Info("delete site command", slog.Int64("chat_id", chatId), slog.String("url", url))
 
-		url, err := urlpkg.ConvertToExpectedUrl(url)
-		if err != nil {
-			slog.Error(
-				"failed to convert url to expected",
-				slog.String("error", err.Error()),
-				slog.String("url", url),
-			)
-			return c.Reply("Invalid URL!")
-		}
-
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
-
-		if err := t.storage.DeleteSite(ctx, chatId, url); err != nil {
-			slog.Error(
-				"failed to delete site",
-				slog.String("command", "delete site"),
-				slog.String("error", err.Error()),
-			)
-			return nil
-		}
-
-		return c.Send("Successful!")
+	url, err := urlpkg.ConvertToExpectedUrl(url)
+	if err != nil {
+		slog.Error(
+			"failed to convert url to expected",
+			slog.String("error", err.Error()),
+			slog.String("url", url),
+		)
+		return c.Reply("Invalid URL!")
 	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := t.storage.DeleteSite(ctx, chatId, url); err != nil {
+		slog.Error(
+			"failed to delete site",
+			slog.String("command", "delete site"),
+			slog.String("error", err.Error()),
+		)
+		return nil
+	}
+
+	return c.Send("Successful!")
 }
 
-func (t *TGBot) listCommand() func(c telebot.Context) error {
-	return func(c telebot.Context) error {
-		slog.Info("list command", slog.Int64("chat_id", c.Chat().ID))
+func (t *TGBot) listCommand(c telebot.Context) error {
+	slog.Info("list command", slog.Int64("chat_id", c.Chat().ID))
 
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
-		sites, err := t.storage.GetAllSitesByChatId(ctx, c.Chat().ID)
-		if err != nil {
-			slog.Error(
-				"failed to get all sites by chat id",
-				slog.String("command", "list"),
-				slog.String("error", err.Error()),
-			)
-			return nil
-		}
-
-		var b strings.Builder
-		for i, site := range sites {
-			fmt.Fprintf(&b, "%d) %s\n", i+1, site.Url)
-		}
-
-		result := b.String()
-		if len(result) == 0 {
-			return c.Send("You are not subscribed to any site")
-		}
-		return c.Send(result)
+	sites, err := t.storage.GetAllSitesByChatId(ctx, c.Chat().ID)
+	if err != nil {
+		slog.Error(
+			"failed to get all sites by chat id",
+			slog.String("command", "list"),
+			slog.String("error", err.Error()),
+		)
+		return nil
 	}
+
+	var b strings.Builder
+	for i, site := range sites {
+		fmt.Fprintf(&b, "%d) %s\n", i+1, site.Url)
+	}
+
+	result := b.String()
+	if len(result) == 0 {
+		return c.Send("You are not subscribed to any site")
+	}
+	return c.Send(result)
 }
